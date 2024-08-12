8954f8e10db263617660ad58868b10a97bdc89ae
alagu018 <155512514+alagu018@users.noreply.github.com>
2024-08-12 14:51:55 +0530

    spicejet

---
+++ src/test/java/Test/TC_001_SignUp.java
@@ -0,0 +1,41 @@
+
+package Test;
+
+import java.awt.AWTException;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import Base.ProjectSpecifications;
+import Page.HomePage;
+import Page.SignUpPage;
+
+
+public class TC_001_SignUp extends ProjectSpecifications{
+	
+    HomePage homePage;
+	SignUpPage signUpPage;	
+	
+	@BeforeTest
+	public void setup() {
+		excelFile = "Sheet";
+		excelSheet = 1;
+	}
+	
+	
+	@Test(dataProvider = "readExcel")
+	public void SignUpTest(String fName,String lName, String DOB, String Number,String email, String password, 
+			String ConfirmPassword) throws InterruptedException, AWTException {
+
+		homePage = new HomePage(driver);
+		click(homePage.signUpButton);
+		Thread.sleep(5000);
+		switchWindows("https://spiceclub.spicejet.com/signup");
+		
+		signUpPage= new SignUpPage(driver);
+		signUpPage.signUp(fName, lName, DOB, Number, email, password, ConfirmPassword);
+		
+		
+
+	}
+}
